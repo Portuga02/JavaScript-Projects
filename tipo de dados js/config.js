6ec2c4ffcecfec530898571f04f07152e490bf71
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2024-02-16 22:05:13 -0300

    Resumos de Javascript

---
+++ tipo de dados js/config.js
@@ -0,0 +1,12 @@
+/*var quantidade = 10;  // numeros inteiros
+var decimal = 4.5; // separados por .
+var string = "Olá em JavaScript"; //  CARACTERES E NOMES
+
+
+var arrayDdos= ["maça"  , "pera", "jaca"]; // array
+
+for(I in arrayDdos){  // UTILZIANDO ARRRAY PARA PERCORRER O VETOR
+  alert( arrayDdos[I]);
+  
+}
+*/
---
+++ tipo de dados js/script_Jquery.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$('button').click(function(){
+		$('h2').hide(); 
+		$('h1').css("color","blue");
+	});
+});
+
+
+
+// metodo CSS modifica a cor dos atributos
+// função HIDE ela esconde uma div
+
+// função click ela chama uma função quando se clica em alguma coisa
