8e4e22686cac267b3d23c7fc7fe1a8205ac8c7e9
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2024-02-21 20:15:59 -0300

    Estudando objetos math

---
+++ AULA-XIV/script.js
@@ -0,0 +1,9 @@
+let num1 = 111 ;
+let num2 = 2.44;
+let num3 = 2.4433312312312333;
+let temporaria = num1  * '5'
+console.log(num1.toString() + 2);
+console.log(num1.toString(22));
+console.log(num3.toFixed(6))
+console.log(Number.isInteger(num1));
+console.log(Number.isNaN(temporaria)) 
