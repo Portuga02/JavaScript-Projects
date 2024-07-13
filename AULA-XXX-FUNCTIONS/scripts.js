7a60a19fcda2681133d1705cf72ea9af79f31a51
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2024-07-13 14:16:07 -0300

    estudando

--- AULA-XXX-FUNCTIONS/scripts.js
+++ AULA-XXX-FUNCTIONS/scripts.js
@@ -2,15 +2,15 @@ function soma(x, y) {
     const resultado = x + y;
     return resultado;// tudo que ta aqui dentro da função está protegido
 
-}
+} 
 
 console.log(soma(1, 2)); // pode ser assim ou
 
-const resultado = soma(2,2);
+const resultado = soma(2, 2);
 console.log(resultado);
 
 //segunda forma, setando valores nos parametros
-function soma2(x = 40, y= 32) {
+function soma2(x = 40, y = 32) {
     const resultado2 = x + y;
     return resultado2;// tudo que ta aqui dentro da função está protegido
 
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Apenas pequenos resumos de javascript no dia a dia e estudos
+vamos estudar pq sabe né
