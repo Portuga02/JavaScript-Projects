175dcb0b43341452ee529c651b21a3028ba35e48
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2024-07-14 14:11:50 -0300

    ajustando

--- AULA-XIV/script.js
+++ AULA-XIV/script.js
@@ -1,7 +1,7 @@
-let num1 = 111 ;
+let num1 = 111;
 let num2 = 2.44;
 let num3 = 2.4433312312312333;
-let temporaria = num1  * '5'
+let temporaria = num1 * '5'
 console.log(num1.toString() + 2);
 console.log(num1.toString(22));
 console.log(num3.toFixed(6))
