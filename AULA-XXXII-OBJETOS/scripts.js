c44c598fa98d49cc543ea7d9e79aa93a499c5c6c
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2024-08-08 22:09:19 -0300

    ajustes

---
+++ AULA-XXXII-OBJETOS/scripts.js
@@ -0,0 +1,22 @@
+const pessoa1 = {
+    nome: 'Avelã',
+    sobrenome: 'Poico',
+    idade: 1,
+
+    // criando metodo dentro do objeto
+    fala() {
+        console.log(`A minha idade atual é ${this.idade}.`)
+    },
+    incrementaIdade() {
+        this.idade++;
+    }
+};
+
+pessoa1.fala();
+pessoa1.incrementaIdade();
+pessoa1.fala();
+pessoa1.incrementaIdade();
+pessoa1.fala();
+pessoa1.incrementaIdade();
+pessoa1.fala();
+pessoa1.incrementaIdade();
